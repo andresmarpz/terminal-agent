8cbac6386e9a42b09b4cb781ff4c087f2cc1f412
andresmarpz <andresmarpz@gmail.com>
2025-04-29 16:50:12 -0300

    include all new routers

--- backend/src/api/v1/index.py
+++ backend/src/api/v1/index.py
@@ -1,11 +1,17 @@
 from fastapi import APIRouter
 
+from src.api.v1.clients.client_router import router as client_router
+from src.api.v1.invoices.invoice_router import router as invoice_router
 from src.api.v1.products.product_router import router as product_router
+from src.api.v1.shipments.shipment_router import router as shipment_router
 from src.core.config import get_settings
 
 api_router_v1 = APIRouter(prefix=get_settings().API_V1_STR)
 
 api_router_v1.include_router(product_router)
+api_router_v1.include_router(client_router)
+api_router_v1.include_router(shipment_router)
+api_router_v1.include_router(invoice_router)
 
 
 @api_router_v1.get("/")
